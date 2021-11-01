487f322fd469f1440c2eb0b8175674485d96d619
diego-tarantola <diego.tarantola2000@gmail.com>
2021-11-01 18:49:09 +0100

    Modifica Elettore

--- progetto/src/main/java/progetto/Elettore.java
+++ progetto/src/main/java/progetto/Elettore.java
@@ -1,6 +1,50 @@
 package progetto;
 
 public class Elettore {
-	String nome;
-	String cognome;
+	
+	private String nome;
+	private String cognome;
+	private String codFiscale;
+	private String cartaId;
+	
+	public Elettore(String nome, String cognome, String codFiscale, String cartaId) {
+		this.setNome(nome);
+		this.setCognome(cognome);
+		this.setCodFiscale(codFiscale);
+		this.setCartaId(cartaId);
+	}
+	
+	public String getNome() {
+		System.out.println("ciao");
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodFiscale() {
+		return codFiscale;
+	}
+
+	public void setCodFiscale(String codFiscale) {
+		this.codFiscale = codFiscale;
+	}
+
+	public String getCartaId() {
+		return cartaId;
+	}
+
+	public void setCartaId(String cartaId) {
+		this.cartaId = cartaId;
+	}
+
 }
