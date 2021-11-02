b7abd5ec07cfd0942fec99148315fab4ff982126
marco <marco.ottaiano@studenti.unimi.it>
2021-11-02 15:16:34 +0100

    Update Scrutatore

--- progetto/src/main/java/progetto/Scrutatore.java
+++ progetto/src/main/java/progetto/Scrutatore.java
@@ -1,5 +1,30 @@
 package progetto;
 
 public class Scrutatore {
+	
+	private String username;
+	private String password;
+	
+	public Scrutatore(String username, String password) {
+		this.setUsername(username);
+		this.setPassword(password);
+	}
+	
+	public String getUsername() {
+		System.out.println("Username");
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
