bf65643f2096d73d9d7180a270247c52a05db162
diego-tarantola <diego.tarantola2000@gmail.com>
2021-11-11 12:14:08 +0100

    Aggiunta attributi di Elettore

--- progetto/src/main/java/progetto/Elettore.java
+++ progetto/src/main/java/progetto/Elettore.java
@@ -4,14 +4,26 @@ public class Elettore {
 	
 	private String nome;
 	private String cognome;
-	private String codFiscale;
-	private String cartaId;
+	private int giornoNascita;
+	private int meseNascita;
+	private int annoNascita;
+	private String luogoNascita;
+	private String sesso;
+	private char[] codFiscale;
+	private boolean voto;
 	
-	public Elettore(String nome, String cognome, String codFiscale, String cartaId) {
+	//non so come gestire l'array di char di codice fiscale
+	
+	public Elettore(String nome, String cognome, int giornoNascita, int meseNascita, int annoNascita, String luogoNascita, String sesso, char[] codFiscale, boolean voto) {
 		this.setNome(nome);
 		this.setCognome(cognome);
+		this.setGiornoNascita(giornoNascita);
+		this.setMeseNascita(meseNascita);
+		this.setAnnoNascita(annoNascita);
+		this.setLuogoNascita(luogoNascita);
 		this.setCodFiscale(codFiscale);
-		this.setCartaId(cartaId);
+		this.setSesso(sesso);
+		this.setVoto(voto);
 	}
 	
 	public String getNome() {
@@ -29,21 +41,67 @@ public class Elettore {
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
+	
+	public int getGiornoNascita() {
+		return giornoNascita;
+	}
 
-	public String getCodFiscale() {
+	public void setGiornoNascita(int giornoNascita) {
+		this.giornoNascita = giornoNascita;
+	}
+
+	public int getMeseNascita() {
+		return meseNascita;
+	}
+
+	public void setMeseNascita(int meseNascita) {
+		this.meseNascita = meseNascita;
+	}
+
+	public int getAnnoNascita() {
+		return annoNascita;
+	}
+
+	public void setAnnoNascita(int annoNascita) {
+		this.annoNascita = annoNascita;
+	}
+	
+	public String getLuogoNascita() {
+		return luogoNascita;
+	}
+
+	public void setLuogoNascita(String luogoNascita) {
+		this.luogoNascita = luogoNascita;
+	}
+
+	public String getSesso() {
+		return sesso;
+	}
+
+	public void setSesso(String sesso) {
+		this.sesso = sesso;
+	}
+
+	public char[] getCodFiscale() {
 		return codFiscale;
 	}
 
-	public void setCodFiscale(String codFiscale) {
+	public void setCodFiscale(char[] codFiscale) {
 		this.codFiscale = codFiscale;
 	}
-
-	public String getCartaId() {
-		return cartaId;
+	
+	public boolean isVoto() {
+		return voto;
 	}
 
-	public void setCartaId(String cartaId) {
-		this.cartaId = cartaId;
+	public void setVoto(boolean voto) {
+		this.voto = voto;
 	}
+	
+	
+	
+	public void esprimi_voto() {}
+
+
 
 }
