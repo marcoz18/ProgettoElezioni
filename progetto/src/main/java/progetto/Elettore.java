4b7a46ffb9cf6e82ee36804379f709ffc268018a
marco <marco.ottaiano@studenti.unimi.it>
2021-11-17 11:28:18 +0100

    Aggiunta metodi checkCodFiscale

--- progetto/src/main/java/progetto/Elettore.java
+++ progetto/src/main/java/progetto/Elettore.java
@@ -5,8 +5,7 @@ public class Elettore extends Persona{
 	private char[] codFiscale;
 	private boolean voto;
 	
-	//non so come gestire l'array di char di codice fiscale
-	public Elettore(String nome, String cognome, int giornoNascita, int meseNascita, int annoNascita, String comuneNascita, String nazioneNascita, String sesso, char[] codFiscale, boolean voto) {
+	public Elettore(String nome, String cognome, int giornoNascita, int meseNascita, int annoNascita, String comuneNascita, String nazioneNascita, String sesso, String codFiscale, boolean voto) {
 		this.setNome(nome);
 		this.setCognome(cognome);
 		this.setGiornoNascita(giornoNascita);
@@ -16,18 +15,14 @@ public class Elettore extends Persona{
 		this.setNazioneNascita(nazioneNascita);
 		this.setSesso(sesso);
 		this.setVoto(voto);
-		
-		this.setCodFiscale(codFiscale); // possiamo fare inserire il codFisc come string e poi usare .toCharArray() per trasformarlo in char[]
+		this.setCodFiscale(codFiscale);
 	}
 
-	public char[] getCodFiscale() {
-		return codFiscale;
-	}
 
-	// ha senso new char[16]? cioè serve a qualcosa? ho risposto sopra 
-	public void setCodFiscale(char[] codFiscale) {
-		codFiscale = new char[16];
-		this.codFiscale = codFiscale;
+
+	public void setCodFiscale(String codFiscale) {
+		char[] codF=codFiscale.toCharArray();
+		this.codFiscale = codF;
 	}
 	
 	public boolean isVoto() {
@@ -40,6 +35,50 @@ public class Elettore extends Persona{
 	
 	public void esprimi_voto() {}
 
+	public boolean checkCodFiscale(char[] cod) {
+		//ciclo per controllare cognome -> 3 lettere
+		if (!checkCogn(cod)) //se è false mi fermo
+			return false;
+		//ciclo per controllare nome -> 3 lettere
+		if (!checkName(cod)) //se è false mi fermo
+			return false;
+		//ciclo per controllare anno di nascita -> 2 numeri
+		if (!checkAnno(cod)) //se è false mi fermo
+			return false;
+		//ciclo per controllare lettera del mese -> 1 lettera
+		if (!checkMese(cod)) //se è false mi fermo
+			return false;
+		//ciclo per controllare giorno di nascita -> 2 numeri
+		if(!checkGiorno(cod)) //se è false mi fermo
+			return false;
+		//controllo comune di nascita--> 1 lettera + 3 numeri
+		if(!checkCom(cod)) //se è false mi fermo
+			return false;
+		//controllo ultimo carattere--> 1 lettera
+		if(!checkLast(cod)) //se è false mi fermo
+			return false;
+		//se tutte le verifiche sono corrette ritorno 'true', altrimenti esco prima con false
+		return true;
+	}
+	
+	public boolean checkLast(char[] cod) {
+		if((cod[15]<'A')&&(cod[15]>'Z'))
+			return false;
+		return true;
+	}
+	
+	public boolean checkCom(char[] cod) {
+		if((cod[11]<'A')&&(cod[11]>'Z'))
+			return false;
+		if(getNazioneNascita()!="Italia")
+			if(cod[11]!='Z')
+				return false;
+		for(int i=12;i<15;i++) {
+			if((cod[i]<'0')&&(cod[i]>'9'))
+				return false;
+		}
+		return true;
+	}
 	
 	public boolean checkAnno(char[] cod) {
 		int anno=this.getAnnoNascita();
@@ -53,59 +92,54 @@ public class Elettore extends Persona{
 	}
 	
 	public boolean checkMese(char[] cod) {
-		/*Qua in realtà volevo usare i caratteri ascii per calcolare le lettere
-		 * fare cod[8]==mese+96 che sarebbe il codice ascii della lettera corrispondente, ma non so come funziona il casting con gli ascii
-		 * perchè cod ti rida un char mentre mese+96 sarebbe un int, se riusciamo a trasformarlo tramite ascii in lettera è gucci, almeno
-		 * tolgo sto switch brutto.
-		 */
 		int mese=this.getMeseNascita();
 		switch(mese) {
 		case 1:
-			if(cod[8]=='a')
+			if(cod[8]=='A')
 				return true;
 			break;
 		case 2:
-			if(cod[8]=='b')
+			if(cod[8]=='B')
 				return true;
 			break;
 		case 3:
-			if(cod[8]=='c')
+			if(cod[8]=='C')
 				return true;
 			break;
 		case 4:
-			if(cod[8]=='d')
+			if(cod[8]=='D')
 				return true;
 			break;
 		case 5:
-			if(cod[8]=='e')
+			if(cod[8]=='E')
 				return true;
 			break;
 		case 6:
-			if(cod[8]=='f')
+			if(cod[8]=='F')
 				return true;
 			break;
 		case 7:
-			if(cod[8]=='g')
+			if(cod[8]=='G')
 				return true;
 			break;
 		case 8:
-			if(cod[8]=='h')
+			if(cod[8]=='H')
 				return true;
 			break;
 		case 9:
-			if(cod[8]=='i')
+			if(cod[8]=='I')
 				return true;
 			break;
 		case 10:
-			if(cod[8]=='l')
+			if(cod[8]=='L')
 				return true;
 			break;
 		case 11:
-			if(cod[8]=='m')
+			if(cod[8]=='M')
 				return true;
 			break;
 		case 12:
-			if(cod[8]=='n')
+			if(cod[8]=='N')
 				return true;
 			break;
 		default:
@@ -146,24 +180,93 @@ public class Elettore extends Persona{
 		}
 	}
 	
-	//il metodo checkCodFiscale potrebbe usare altri metodi per aumentare l'astrazione ? direi di si
-	public boolean checkCodFiscale(char[] cod) {
-		//ciclo per controllare cognome -> 3 lettere
-
-		//ciclo per controllare nome -> 3 lettere
-		
-		//ciclo per controllare anno di nascita -> 2 numeri
-		if (!checkAnno(cod)) //se è false mi fermo
-			return false;
-		//ciclo per controllare lettera del mese -> 1 lettera
-		if (!checkMese(cod)) //se è false mi fermo
-			return false;
-		//ciclo per controllare giorno di nascita -> 2 numeri
-		if(!checkGiorno(cod)) //se è false mi fermo
-			return false;
-		//se tutte le verifiche sono corrette ritorno 'true', altrimenti esco prima con false
+	public boolean checkCogn(char[] cod) {
+		String surn=this.getCognome().toUpperCase();
+		char[] surname=surn.toCharArray();
+		int j=0,k=0;
+		for(int i=0;i<3;i++) {
+			while(j<surname.length) {
+				if((surname[j]!='A')||(surname[j]!='E')||(surname[j]!='I')||(surname[j]!='O')||(surname[j]!='U')) {
+					if(cod[i]!=surname[j])
+						return false;
+					j++;
+					break;
+				}
+				j++;
+			}
+			if(j>=surname.length){
+				while(k<surname.length) {
+					if((surname[k]=='A')||(surname[k]=='E')||(surname[k]=='I')||(surname[k]=='O')||(surname[k]=='U')) {
+						if(cod[i]!=surname[k])
+							return false;
+						k++;
+						break;
+					}
+					k++;
+				}
+			}
+			if((j>=surname.length)&&(k>=surname.length)) {
+				if(cod[i]!='X')
+					return false;
+			}
+		}
 		return true;
 	}
-
-
+	
+	public boolean checkName(char[] cod) {
+		String nome=this.getNome().toUpperCase();
+		char[] name=nome.toCharArray();
+		int j=0,k=0,cons=0;
+		//controllo # consonanti
+		for(int x=0;x<name.length;x++) {
+			if((name[x]!='A')||(name[x]!='E')||(name[x]!='I')||(name[x]!='O')||(name[x]!='U')) {
+				cons++;
+			}
+		}
+		if(cons<=3) {
+			for(int i=3;i<6;i++) {
+				while(j<name.length) {
+					if((name[j]!='A')||(name[j]!='E')||(name[j]!='I')||(name[j]!='O')||(name[j]!='U')) {
+						if(cod[i]!=name[j])
+							return false;
+						j++;
+						break;
+					}
+					j++;
+				}
+				if(j>=name.length){
+					while(k<name.length) {
+						if((name[k]=='A')||(name[k]=='E')||(name[k]=='I')||(name[k]=='O')||(name[k]=='U')) {
+							if(cod[i]!=name[k])
+								return false;
+							k++;
+							break;
+						}
+						k++;
+					}
+				}
+				if((j>=name.length)&&(k>=name.length)) {
+					if(cod[i]!='X')
+						return false;
+				}
+			}
+		}else {
+			cons=0;
+			for(int i=3;i<6;i++) {
+				while(j<name.length) {
+					if((name[j]!='A')||(name[j]!='E')||(name[j]!='I')||(name[j]!='O')||(name[j]!='U')) {
+						cons++;
+						if(cons!=2) {
+							if(cod[i]!=name[j]) 
+								return false;
+							j++;
+							break;
+						}
+					}
+					j++;
+				}
+			}
+		}
+		return true;
+	}
 }
